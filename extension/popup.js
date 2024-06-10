7c4c73680c646b93a80f939f172860f50e6a2bc5
Sagun Devkota <sagundevyt@gmail.com>
2024-06-10 23:06:29 +0545

    discarded demo changes

--- extension/popup.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-    console.log(message)
-    if (message.popupMessage) {
-      // update popup DOM
-      document.getElementById("message").innerHTML = message.popupMessage
-    }
-  });
