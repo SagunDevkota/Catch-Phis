c50e09da0984edc178063b5331a22da1890b2ce2
Prajwalgautam <prajwalgautam007@gmail.com>
2024-08-19 19:56:09 +0545

    corporate added

---
+++ frontend/src/components/Corporate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Corporate = () => {
+    return (
+        <div>
+            <h2>corporate</h2>
+            <p></p>
+        </div>
+    );
+}
+
+export default Corporate;
