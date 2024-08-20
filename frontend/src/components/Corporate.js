5cceebf24d8e68a947ecd89635f6c6c07fa43ee1
Prajwalgautam <prajwalgautam007@gmail.com>
2024-08-20 10:41:06 +0545

    corporate api fetch

--- frontend/src/components/Corporate.js
+++ frontend/src/components/Corporate.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const Corporate = () => {
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/corporate/corporate/detail')
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error('There was an error fetching the data!', error);
+          });
+      }, []);
     return (
         <div>
             <h2>corporate</h2>
