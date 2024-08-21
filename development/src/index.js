2ebc3abfcf79bf894a115cd2f2efd2832a1a5fb1
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2024-08-21 18:54:03 +0545

    user, dashboard module complete

--- development/src/index.js
+++ development/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import "./asset/styles/index.css";
-import "./asset/styles/bootstrap.custom.css";
+import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './modules/users/context/AuthContext';
+/*
+  Bootstrap configuration
+*/
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+/*
+  Bootstrap icons configuration
+*/
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+/*
+  React toastify configuration
+*/
+import '../node_modules/react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  // <React.StrictMode>
+    <AuthProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </AuthProvider>
+  // </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
