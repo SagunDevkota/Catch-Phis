94da4b4a4213cd394a66fbd1d63246cb570f6c09
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2024-08-21 18:48:47 +0545

    +error utilities

---
+++ development/src/util/ErrorMessage.js
@@ -0,0 +1,16 @@
+let ErrorMessage = (props) => {
+  console.log(props.message);
+  return (
+    <>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col">
+            {/* <p className="h3 text-danger">{props.message}</p> */}
+            <div className="alert alert-danger" role="alert">{props.message}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ErrorMessage;
